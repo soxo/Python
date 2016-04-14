1b3bdf146bd51170251a8819a51a85912b5b8f65
soxo <919006445@qq.com>
2016-04-14 16:16:00 +0800

    add log

--- script/find_file.py
+++ script/find_file.py
@@ -2,10 +2,17 @@
 # -*- coding:utf-8 -*-
 
 
+import os,re
+
 
 def find():
-    fileName = raw_input('fileName: ')
-    pwd = chdir('/')
-    print pwd
-    
+    fileName = raw_input('FileName: ')
+    reobj = re.compile(fileName)
+    for pwd,dirs,name in os.walk('/'):
+        if fileName in name:
+            print "%s/%s" % (pwd,fileName)
+         
 
+
+
+find()
