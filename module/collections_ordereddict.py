9c13b8a36fa62afec1f9bb967b19f4ef51a5c7a0
soxo <919006445@qq.com>
2016-04-15 13:27:09 +0800

    add module collections

---
+++ module/collections_ordereddict.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+
+
+import collections
+
+dic = collections.OrderedDict([('a',1),('b',2),('c',3)])
+print 'Key: %s'  % dic.keys()
+print 'Values: %s' % dic.values()
+print 'Items: %s'   % dic.items()
+print  'a: %s'       % dic['a']
+
+
+
+
+
+
+
