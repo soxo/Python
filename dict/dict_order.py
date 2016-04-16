5c46682d415a454798c89f668f812cb96521dd19
soxo <919006445@qq.com>
2016-04-16 18:22:55 +0800

    add dict_order

---
+++ dict/dict_order.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+
+# Script Name   : sort_reverse.py
+# Author        : Zhang Dong  
+# Last Modified :  
+# Version       : 1.0 
+# Last Modified : 
+
+
+
+dic = {'a':1,'b':2,'c':3}
+print 'old dict: %s' % dic
+key = dic.keys()
+key.sort()
+for i in key:
+    print '%s => %s' % (i,dic[i])
+
+
+print '*' * 40
+
+dic = {'a':1,'b':2,'c':3}
+print 'old dict: %s' % dic
+for key in sorted(dic):
+    print '%s => %s' % (key,dic[key])
+
