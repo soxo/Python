dd73a32f35fc06a763b13302ec046fa459c7f4c4
soxo <919006445@qq.com>
2016-04-18 11:06:52 +0800

    add log

---
+++ dict/in_get.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+
+# Script Name   : in_get.py 
+# Author        : Zhang Dong  
+# Last Modified :  
+# Version       : 1.0 
+# Last Modified :  
+
+
+dic = {'soxo':'one','admin':'two','python':'three'}
+print 'linux' in dic
+print 'soxo' in dic
+
+
+print dic.get('linux','nothing...')
+print dic.get('soxo','nothing......')
+
+print '*' * 40
+
+m = dic['linux'] if 'linux' in dic else 'nothing...'
+print m
+
+n = dic['soxo'] if 'soxo' in dic else 'nothing.....' 
+print n
+
+print  '-' * 40
+d =  'soxo' in dic and dic['soxo'] or 'nothing...'
+print d 
---
+++ test/raw_slice.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+
+
+s = r'c:\admin\unix\\'[:-1]
+print s
