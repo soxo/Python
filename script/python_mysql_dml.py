fe700257fc1fdd16bddc5648abc15acbdff2479c
soxo <919006445@qq.com>
2016-04-18 17:38:25 +0800

    add log

--- script/python_mysql_dml.py
+++ script/python_mysql_dml.py
@@ -17,7 +17,7 @@ import MySQLdb
 conn = MySQLdb.connect(host='localhost',user='root',passwd='      ')
 cur = conn.cursor()
 cur.execute('create database  if not exists python')
-cur.select _db('use python')
+conn.select_db('python')
 cur.execute('select  database()')
 print cur.fetchall()
 
