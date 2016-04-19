355be95331fb12cf7b87cce3eabbb4e52c717084
soxo <919006445@qq.com>
2016-04-19 13:31:42 +0800

    add log

---
+++ loop/while_else.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+
+
+while True:
+    
+    x = [i for i in range(10) if i%2 == 0]
+    print x 
+    break
+    
+
+else:
+    print "It's over"
+
+
+print "end"
